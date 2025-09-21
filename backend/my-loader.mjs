aba46a803c1b61ed64f5a9a8e6097e48498033db
Julio Flores <cesarff.julio@gmail.com>
2025-09-20 22:09:08 -0600

    feat: :art: configuración de rutas

---
+++ backend/my-loader.mjs
@@ -0,0 +1,11 @@
+import generateAliasesResolver from "esm-module-alias";
+
+const aliases = {
+    "@config": "./src/config",
+    "@controllers": "./src/controllers",
+    "@models": "./src/models",
+    "@routes": "./src/routes",
+    "@utils": "./src/utils"
+};
+
+export const resolve = generateAliasesResolver(aliases);
